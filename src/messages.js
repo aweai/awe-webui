06b0f727bbfd2ae7af4e4bb82c4fe2929aee7778
Pedro <awe.ai.pedro@gmail.com>
2025-02-10 09:01:39 +0800

    [feature] clear previous alert before showing new

--- src/messages.js
+++ src/messages.js
@@ -1,15 +1,19 @@
 
 export const alert = (message, type, duration) => {
     const alertPlaceholder = document.getElementById('global-alert')
-    const wrapper = document.createElement('div')
-    wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
-    alertPlaceholder.append(wrapper)
+    alertPlaceholder.innerHTML = ""
 
-    if(duration) {
-        setTimeout(() => {
-            alertPlaceholder.innerHTML = ""
-        }, duration)
-    }
+    setTimeout(() => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+        alertPlaceholder.append(wrapper)
+
+        if(duration) {
+            setTimeout(() => {
+                alertPlaceholder.innerHTML = ""
+            }, duration)
+        }
+    }, 10)
 }
 
 export const waiting = (message) => {
